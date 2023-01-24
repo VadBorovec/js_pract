99ce60840ecf6bdbc71dd2482dae0d79ae826c30
VadBorovec <vadborov@gmail.com>
2023-01-25 00:13:50 +0200

    add script to raise numbers to a power

---
+++ js/numbers.js
@@ -0,0 +1,10 @@
+let base = prompt('Add number');
+base = Number(base);
+console.log(base);
+
+let power = prompt('Add power');
+power = Number(power);
+console.log(power);
+
+const result = base ** power;
+console.log(result);
