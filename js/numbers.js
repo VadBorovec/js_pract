0f8486549b9852d594cd097a24afdc7ab33d112c
VadBorovec <vadborov@gmail.com>
2023-01-27 23:17:20 +0200

    update numbers.js and input.js

--- js/numbers.js
+++ js/numbers.js
@@ -1,3 +1,42 @@
+// Парс числа с Number.parseInt() и Number.parseFloat()
+
+
+let elementWidth = '50.25px';
+const result = Number.parseInt(elementWidth);
+console.log('elementWidth:', result);
+
+let elementHeight = '200.74px';
+elementHeight = Number.parseFloat(elementHeight);
+console.log('elementHeight:', elementHeight);
+  
+
+// Метод число. toFixed(digits)
+// Матрёшки на примере console. Log(Number( число. toFtxed(digits)))
+
+const salary = 1300.16472;
+console.log(salary.toFixed());
+console.log(salary);
+
+// OR
+
+let salary2 = 1300.16472;
+salary2 = Number(salary2.toFixed(2));
+
+console.log(salary2);
+
+
+// Приведение (преобразование) к числу с Number(value)
+// Значение NaN (Not a Number) и метод Number .isNaN(value)
+
+let quantity = '30';
+let value = 'Эту строку невозможно привести к числу';
+
+console.log(Number(quantity));
+console.log(Number(value));
+console.log(Number(5 / 0));
+
+
+// Об'єкт Math
 // піднесення числа до степеня
 
 // let base = prompt('Add number');
@@ -12,7 +51,7 @@
 // console.log(result);
 
 
-// генератор випадкових чисел
+// генератор випадкових чисел Math.random() Math.round()
 
 // const max = 50;
 // const min = 30;
