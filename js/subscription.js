b512de7e7bc6fa4c111f83ef3f8ee98bc4045c18
VadBorovec <vadborov@gmail.com>
2023-01-27 23:58:34 +0200

    add script subscription.js

---
+++ js/subscription.js
@@ -0,0 +1,8 @@
+// Напиши скрипт проверки подписки пользователя при доступе к контенту
+// - Есть три типа подписки: free, pro и vip.
+// - Получить доступ могут только пользователи pro и vip
+
+const subscription = 'pro';
+
+//  const canAccessContent = ;
+// console.log('Есть доступ K контенту? ', canAccessContent);
