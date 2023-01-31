c879db6f675b3e74e54b552008f82c6bf693bfb3
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-01-31 23:45:11 +0200

    Add 21-26 tasks to autocheck.js

--- js/15-autocheck.js
+++ js/15-autocheck.js
@@ -13,13 +13,13 @@
 // Виклик calculateTotalPrice(12, 70) повертає 840
 // Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
 
-function calculateTotalPrice(orderedQuantity, pricePerItem) {
-  // Change code below this line
-    const totalPrice = orderedQuantity * pricePerItem;
-    console.log(totalPrice);
-  // Change code above this line
-  return totalPrice;
-};
+// function calculateTotalPrice(orderedQuantity, pricePerItem) {
+//   // Change code below this line
+//     const totalPrice = orderedQuantity * pricePerItem;
+//     console.log(totalPrice);
+//   // Change code above this line
+//   return totalPrice;
+// };
 
 // calculateTotalPrice(5, 100);
 // calculateTotalPrice(8, 60);
@@ -42,15 +42,15 @@ function calculateTotalPrice(orderedQuantity, pricePerItem) {
 // Виклик makeOrderMessage(4, 300, 100) повертає "You ordered droids worth 1300 credits. Delivery (100 credits) is included in total price."
 // Виклик makeOrderMessage(10, 70, 200) повертає "You ordered droids worth 900 credits. Delivery (200 credits) is included in total price."
 
-function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
-    // Change code below this line
-    const message = `You ordered droids worth ${orderedQuantity * pricePerDroid + deliveryFee} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//     // Change code below this line
+//     const message = `You ordered droids worth ${orderedQuantity * pricePerDroid + deliveryFee} credits. Delivery (${deliveryFee} credits) is included in total price.`;
     
-  // Change code above this line
-  return message;
-}
+//   // Change code above this line
+//   return message;
+// }
 
-let result = makeOrderMessage(2, 100, 50);
+// let result = makeOrderMessage(2, 100, 50);
 // console.log(result);
 // result = makeOrderMessage(4, 300, 100);
 // console.log(result);
@@ -69,13 +69,13 @@ let result = makeOrderMessage(2, 100, 50);
 // Виклик isAdult(8) повертає false
 // Виклик isAdult(37) повертає true
 
-function isAdult(age) {
-  // Change code below this line
-  const passed = age >= 18;
+// function isAdult(age) {
+//   // Change code below this line
+//   const passed = age >= 18;
 
-  // Change code above this line
-  return passed;
-}
+//   // Change code above this line
+//   return passed;
+// }
 
 // result = isAdult(20);
 // console.log(result);
@@ -99,14 +99,14 @@ function isAdult(age) {
 // Виклик isValidPassword("kiwirul3z") повертає false
 // Виклик isValidPassword("jqueryismyjam") повертає true
 
-function isValidPassword(password) {
-  const SAVED_PASSWORD = 'jqueryismyjam';
-  // Change code below this line
-  const isMatch = password === SAVED_PASSWORD;
+// function isValidPassword(password) {
+//   const SAVED_PASSWORD = 'jqueryismyjam';
+//   // Change code below this line
+//   const isMatch = password === SAVED_PASSWORD;
 
-  // Change code above this line
-  return isMatch;
-}
+//   // Change code above this line
+//   return isMatch;
+// }
 
 // result = isValidPassword("mangodab3st");
 // console.log(result);
@@ -129,17 +129,17 @@ function isValidPassword(password) {
 // Виклик checkAge(14) повертає "You are a minor"
 // Виклик checkAge(38) повертає "You are an adult"
 
-function checkAge(age) {
-  let message;
+// function checkAge(age) {
+//   let message;
 
-  if (age >= 18) { // Change this line
-    message = 'You are an adult';
-  } else {
-    message = 'You are a minor';
-  }
+//   if (age >= 18) { // Change this line
+//     message = 'You are an adult';
+//   } else {
+//     message = 'You are a minor';
+//   }
 
-  return message;
-}
+//   return message;
+// }
 
 // result = checkAge(20);
 // console.log(result);
@@ -206,16 +206,16 @@ function checkAge(age) {
 // Значення змінної d дорівнює 2
 // Використаний оператор /=
 
-let a = 5;
-let b = 10;
-let c = 15;
-let d = 20;
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
 
-// Change code below this line
-a += 2;
-b -= 4;
-c *= 3;
-d /= 10;
+// // Change code below this line
+// a += 2;
+// b -= 4;
+// c *= 3;
+// d /= 10;
 
 // console.log(a, b, c, d);
 
@@ -242,20 +242,20 @@ d /= 10;
 // Виклик makeTransaction(2000, 8, 10000) повертає "Insufficient funds!"
 // Виклик makeTransaction(500, 10, 5000) повертає "You ordered 10 droids, you have 0 credits left"
 
-function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
-  let message;
-  // Change code below this line
-    let totalPrice = pricePerDroid * orderedQuantity;
-    if (totalPrice > customerCredits) {
-        message = 'Insufficient funds!';
-    } else {
-        customerCredits -= totalPrice;
-        message = `You ordered ${orderedQuantity} droids, you have ${customerCredits} credits left`;
-    }
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//   let message;
+//   // Change code below this line
+//     let totalPrice = pricePerDroid * orderedQuantity;
+//     if (totalPrice > customerCredits) {
+//         message = 'Insufficient funds!';
+//     } else {
+//         customerCredits -= totalPrice;
+//         message = `You ordered ${orderedQuantity} droids, you have ${customerCredits} credits left`;
+//     }
 
-  // Change code above this line
-  return message;
-}
+//   // Change code above this line
+//   return message;
+// }
 
 // result = makeTransaction(3000, 5, 23000);
 // console.log(result);
@@ -282,20 +282,20 @@ function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
 // Виклик checkPassword("polyhax") повертає "Access denied, wrong password!"
 // Виклик checkPassword("jqueryismyjam") повертає "Welcome!"
 
-function checkPassword(password) {
-  const ADMIN_PASSWORD = 'jqueryismyjam';
-  let message;
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   let message;
 
-  if (password === null) { // Change this line
-    message =  'Canceled by user!';
-  } else if (password === ADMIN_PASSWORD) { // Change this line
-    message = 'Welcome!';
-  } else {
-    message = 'Access denied, wrong password!';
-  }
+//   if (password === null) { // Change this line
+//     message =  'Canceled by user!';
+//   } else if (password === ADMIN_PASSWORD) { // Change this line
+//     message = 'Welcome!';
+//   } else {
+//     message = 'Access denied, wrong password!';
+//   }
 
-  return message;
-}
+//   return message;
+// }
 
 // result = checkPassword("mangohackzor");
 // console.log(result);
@@ -327,30 +327,311 @@ function checkPassword(password) {
 // Виклик checkStorage(200, 250) повертає "Your order is too large, there are not enough items in stock!"
 // Виклик checkStorage(150, 0) повертає "There are no products in the order!"
 
-function checkStorage(available, ordered) {
-  let message;
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Change code below this line
+//   if (ordered === 0) {
+//     message = 'There are no products in the order!';
+//   } else if (ordered > available) {
+//     message = 'Your order is too large, there are not enough items in stock!';
+//   } else {
+//     message = 'The order is accepted, our manager will contact you';
+//   }
+
+//   // Change code above this line
+//   return message;
+// }
+
+// result = checkStorage(100, 50);
+// console.log(result);
+// result = checkStorage(100, 130);
+// console.log(result);
+// result = checkStorage(70, 0);
+// console.log(result);
+// result = checkStorage(200, 20);
+// console.log(result);
+// result = checkStorage(200, 250);
+// console.log(result);
+// result = checkStorage(150, 0);
+// console.log(result);
+
+// =========================================================================================
+
+// 21
+
+// Функція isNumberInRange(start, end, number) перевіряє, чи входить число у проміжок. Вона оголошує три параметри, значення яких будуть задаватися під час її виклику:
+
+// number - число, входження якого перевіряється
+// start - початок числового проміжку
+// end - кінець числового проміжку
+// Присвой змінній isInRange вираз перевірки входження number у числовий проміжок від start до end. Тобто число повинно бути більшим або дорівнювати start, і меншим або дорівнювати end. Результатом виразу перевірки буде буль true або false.
+
+// Оголошена функція isNumberInRange(start, end, number)
+// У виразі перевірки використаний оператор &&
+// Виклик isNumberInRange(10, 30, 17) повертає true
+// Виклик isNumberInRange(10, 30, 5) повертає false
+// Виклик isNumberInRange(20, 50, 24) повертає true
+// Виклик isNumberInRange(20, 50, 76) повертає false
+
+// function isNumberInRange(start, end, number) {
+//   const isInRange = number >= start && number <= end; // Change this line
+
+//   return isInRange;
+// }
+
+// result = isNumberInRange(10, 30, 17);
+// console.log(result);
+// result = isNumberInRange(10, 30, 5);
+// console.log(result);
+// result = isNumberInRange(20, 50, 24);
+// console.log(result);
+// result = isNumberInRange(20, 50, 76);
+// console.log(result);
+
+// ===============================================================================================
+
+// 22
+
+// Функція checkIfCanAccessContent(subType) перевіряє, чи може користувач отримати доступ до контенту. Перевірка відбувається за типом передплати. Отримати доступ можуть тільки користувачі з передплатою pro або vip.
+
+// Присвой змінній canAccessContent вираз перевірки передплати. Якщо значення параметра subType дорівнює рядкам "pro" або "vip", користувач отримає доступ. Результатом виразу перевірки буде буль true або false.
+
+// Оголошена функція checkIfCanAccessContent(subType)
+// У виразі перевірки використаний оператор ||
+// Виклик checkIfCanAccessContent("pro") повертає true
+// Виклик checkIfCanAccessContent("starter") повертає false
+// Виклик checkIfCanAccessContent("vip") повертає true
+// Виклик checkIfCanAccessContent("free") повертає false
+
+// function checkIfCanAccessContent(subType) {
+//   const canAccessContent = subType === 'pro' || subType === 'vip' ; // Change this line
+
+//   return canAccessContent;
+// }
+
+// result = checkIfCanAccessContent("pro");
+// console.log(result);
+// result = checkIfCanAccessContent("starter");
+// console.log(result);
+// result = checkIfCanAccessContent("vip");
+// console.log(result);
+// result = checkIfCanAccessContent("free");
+// console.log(result);
+
+// ===============================================================================================
+
+// 23
+
+// Функція isNumberNotInRange(start, end, number) перевіряє, чи не входить число у проміжок. Тобто число повинно бути меншим або дорівнювати start і більшим або дорівнювати end. Результатом виразу перевірки буде буль true або false.
+
+// Вона оголошує три параметри, значення яких будуть задаватися під час її виклику:
+
+// number - число, невходження якого перевіряється
+// start - початок числового проміжку
+// end - кінець числового проміжку
+// Присвой змінній isNotInRange вираз інверсії значення змінної isInRange, використовуючи оператор !.
+
+// Оголошена функція isNumberNotInRange(start, end, number)
+// У виразі використаний оператор !
+// Виклик isNumberNotInRange(10, 30, 17) повертає false
+// Виклик isNumberNotInRange(10, 30, 5) повертає true
+// Виклик isNumberNotInRange(20, 50, 24) повертає false
+// Виклик isNumberNotInRange(20, 50, 76) повертає true
+
+// function isNumberNotInRange(start, end, number) {
+//   const isInRange = number >= start && number <= end;
+//   const isNotInRange = !isInRange ; // Change this line
+
+//   return isNotInRange;
+// }
+
+// result = isNumberNotInRange(10, 30, 17);
+// console.log(result);
+// result = isNumberNotInRange(10, 30, 5);
+// console.log(result);
+// result = isNumberNotInRange(20, 50, 24);
+// console.log(result);
+// result = isNumberNotInRange(20, 50, 76);
+// console.log(result);
+
+// ==============================================================================================
+
+// 24
+
+// Функція getDiscount(totalSpent) визначає значення знижки, залежно від загальної суми витрачених грошей (параметр totalSpent) в магазині за весь час (партнерська програма). Знижка записується у змінну discount і повертається з функції як результат її роботи.
+
+// Використовуючи розгалуження і логічні оператори, доповни код функції.
+
+// Якщо витрачено від 50000 (включно) або більше кредитів - знижка 10% (золотий партнер)
+// Якщо витрачено від 20000 (включно) до 50000 кредитів - знижка 5% (срібний партнер)
+// Якщо витрачено від 5000 (включно) до 20000 кредитів - знижка 2% (бронзовий партнер)
+// Якщо витрачено менше 5000 кредитів - знижка 0 (базовий партнер)
+// Значення знижок кожного рівня зберігаються в однойменних константах BASE_DISCOUNT, BRONZE_DISCOUNT, SILVER_DISCOUNT і GOLD_DISCOUNT.
+
+// Оголошена функція getDiscount(totalSpent)
+// Виклик getDiscount(137000) повертає 0.1
+// Виклик getDiscount(46900) повертає 0.05
+// Виклик getDiscount(8250) повертає 0.02
+// Виклик getDiscount(1300) повертає 0
+// Виклик getDiscount(5000) повертає 0.02
+// Виклик getDiscount(20000) повертає 0.05
+// Виклик getDiscount(50000) повертає 0.1
+
+// function getDiscount(totalSpent) {
+//   const BASE_DISCOUNT = 0;
+//   const BRONZE_DISCOUNT = 0.02;
+//   const SILVER_DISCOUNT = 0.05;
+//   const GOLD_DISCOUNT = 0.1;
+//   let discount;
+//   // Change code below this line
+
+//   if (totalSpent >= 50000) {
+//     discount = GOLD_DISCOUNT;
+//   } else if (totalSpent >= 20000 && totalSpent < 50000) {
+//     discount = SILVER_DISCOUNT;
+//   } else if (totalSpent >= 5000 && totalSpent < 20000) {
+//     discount = BRONZE_DISCOUNT;
+//   } else {
+//     discount = BASE_DISCOUNT;
+//   }
+
+//   // Change code above this line
+//   return discount;
+// }
+
+// result = getDiscount(137000);
+// console.log(result);
+// result = getDiscount(46900);
+// console.log(result);
+// result = getDiscount(8250);
+// console.log(result);
+// result = getDiscount(1300);
+// console.log(result);
+// result = getDiscount(5000);
+// console.log(result);
+// result = getDiscount(20000);
+// console.log(result);
+// result = getDiscount(50000);
+// console.log(result);
+
+// =====================================================================================
+
+// 25
+
+// Виконай рефакторинг рішення задачі «Склад товарів», замінивши інструкцію if...else тернарним оператором.
+
+// Оголошена функція checkStorage(available, ordered).
+// Використаний тернарний оператор.
+// Виклик checkStorage(100, 50) повертає "The order is accepted, our manager will contact you"
+// Виклик checkStorage(100, 130) повертає "Not enough goods in stock!"
+// Виклик checkStorage(200, 20) повертає "The order is accepted, our manager will contact you"
+// Виклик checkStorage(200, 150) повертає "The order is accepted, our manager will contact you"
+// Виклик checkStorage(150, 180) повертає "Not enough goods in stock!"
+
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Change code below this line
+
+//     message = ordered > available ? "Not enough goods in stock!" : "The order is accepted, our manager will contact you";
+
+//   // Change code above this line
+//   return message;
+// }
+
+// result = checkStorage(100, 50);
+// console.log(result);
+// result = checkStorage(100, 130);
+// console.log(result);
+// result = checkStorage(200, 20);
+// console.log(result);
+// result = checkStorage(200, 150);
+// console.log(result);
+// result = checkStorage(150, 180);
+// console.log(result);
+
+// ===========================================================================================================
+
+// 26
+
+// Функція checkPassword(password) порівнює переданий їй пароль (параметр password) зі збереженим паролем адміністратора (константа ADMIN_PASSWORD) і повертає рядок з повідомленням про результат.
+
+// Використовуючи тернарний оператор, доповни функцію таким чином, що:
+
+// Якщо значення password і ADMIN_PASSWORD збігаються, присвой змінній message рядок "Access is allowed".
+// В іншому випадку, присвой message рядок "Access denied, wrong password!".
+// Оголошена функція checkPassword(password)
+// Використаний тернарний оператор
+// Виклик checkPassword("jqueryismyjam") повертає "Access is allowed"
+// Виклик checkPassword("angul4r1sl1f3") повертає "Access denied, wrong password!"
+// Виклик checkPassword("r3actsux") повертає "Access denied, wrong password!"
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   let message;
+//   // Change code below this line
+
+//   message = password === ADMIN_PASSWORD ? "Access is allowed" : "Access denied, wrong password!";
+
+//   // Change code above this line
+//   return message;
+// }
+
+// let result = checkPassword("jqueryismyjam");
+// console.log(result);
+// result = checkPassword("angul4r1sl1f3");
+// console.log(result);
+// result = checkPassword("r3actsux");
+// console.log(result);
+
+// ======================================================================================
+
+// 27
+
+// Функція getSubscriptionPrice(type) отримує рядок з типом передплати користувача (параметр type), перевіряє її на збіг з трьома можливими типами щомісячної передплати та повертає ціну, що зберігається у змінній price.
+
+// Якщо значення параметра type - це рядок:
+
+// "starter" - ціна передплати 0 кредитів.
+// "professional" - ціна передплати 20 кредитів.
+// "organization" - ціна передплати 50 кредитів.
+// Спочатку в тілі функції була інструкція if...else, яка виглядала ось так.
+
+// if (type === "starter") {
+//   price = 0;
+// } else if (type === "professional") {
+//   price = 20;
+// } else if (type === "organization") {
+//   price = 50;
+// }
+// Після рефакторингу інструкція if..else була замінена на switch. Доповни код інструкції switch, щоб функція працювала правильно.
+
+// Оголошена функція getSubscriptionPrice(type)
+// Виклик getSubscriptionPrice("professional") повертає число 20
+// Виклик getSubscriptionPrice("organization") повертає число 50
+// Виклик getSubscriptionPrice("starter") повертає число 0
+
+function getSubscriptionPrice(type) {
+  let price;
   // Change code below this line
-  if (ordered === 0) {
-    message = 'There are no products in the order!';
-  } else if (ordered > available) {
-    message = 'Your order is too large, there are not enough items in stock!';
-  } else {
-    message = 'The order is accepted, our manager will contact you';
+
+ switch () { // Change this line
+    case : // Change this line
+      price = ; // Change this line
+      break;
+
+    case : // Change this line
+      price = ; // Change this line
+      break;
+
+    case : // Change this line
+      price = ; // Change this line
+      break;
   }
 
   // Change code above this line
-  return message;
+  return price;
 }
 
-result = checkStorage(100, 50);
-console.log(result);
-result = checkStorage(100, 130);
-console.log(result);
-result = checkStorage(70, 0);
-console.log(result);
-result = checkStorage(200, 20);
-console.log(result);
-result = checkStorage(200, 250);
-console.log(result);
-result = checkStorage(150, 0);
+
+let result = checkPassword("jqueryismyjam");
 console.log(result);
