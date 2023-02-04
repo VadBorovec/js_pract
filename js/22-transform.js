857529d6eec043bd4512743388c382e9f34ec224
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-02-05 00:59:27 +0200

    update transform.js

---
+++ js/22-transform.js
@@ -0,0 +1,23 @@
+// Напиши скрипт, который объединяет все элементы массива в одно страковое
+// значение.
+// - Элементов может быть произвольное кол-во.
+// - Пусть элементы массива в строке будут разделены запятой.
+// - Сначала через for
+// - Потом через join()
+// Должно получиться 'Mango, Poly, Kiwi, Ajax'
+     
+// // FOR
+// const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+// let string = ' ';
+
+// for (const friend of friends) {
+//     string += friend + ', ';
+// }
+// string = string.slice(0, string.length - 2);
+// console.log(string);
+
+// // JOIN()
+// const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+// const string = friends.join(', ');
+
+// console.log(string);
