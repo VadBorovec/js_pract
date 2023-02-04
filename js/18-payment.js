9308892da52b03aa887146a56e50d58ab0865f57
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-02-04 19:47:26 +0200

    update payment.js

---
+++ js/18-payment.js
@@ -0,0 +1,21 @@
+// Порахувати загальну суму покупок в магазині
+
+// const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+// // 2 сделать переменную total до цикла
+// let total = 0;
+// // 1 перебрать массив
+// for (let i = 0; i < cart.length; i += 1) {
+//     console.log(cart[i]);
+//     // 3 каждый элемент приплюслвать к total
+//     total += cart[i];
+// }
+// console.log('Total', total);
+
+
+// Це ж завдвння через for.. of
+const carts = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+let total = 0;
+for (let cart of carts) {
+    total += cart;
+}
+console.log('Total', total);
