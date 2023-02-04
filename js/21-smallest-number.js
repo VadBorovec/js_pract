8ad16b22d152cdfb25a9da5cedf043936e5f4582
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-02-05 00:43:46 +0200

    update smallest-number.js

---
+++ js/21-smallest-number.js
@@ -0,0 +1,17 @@
+// Напиши скрипт пошуку найменшого числа в масиві,
+// за умови, що числа унікальні (не повторюються)
+
+// const numbers = [51, 13, 24, 7, 85, 19];
+// let smallestNumber = numbers[0];
+// // console.log('firstly :', smallestNumber);
+// // let message = 'is smallest';
+// for (const number of numbers) {
+//     // console.log('remote :', number);
+//     if (number < smallestNumber) {
+//         smallestNumber = number;
+//         // message = `${number} smallest`;
+//         // console.log(message);
+//         // continue;
+//     }
+// }
+// console.log('Smallest Number :', smallestNumber);
