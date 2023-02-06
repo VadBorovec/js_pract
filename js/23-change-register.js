2299f2db60830c8a16c5ca22c0176c401f1cf458
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-02-06 21:38:14 +0200

    Update change-register.js

--- js/23-change-register.js
+++ js/23-change-register.js
@@ -1,1 +1,45 @@
-Напиши скрипт, який заміняє оегістр кожного смволу в рядку на протилежний
+// Напиши скрипт, який заміняє регістр кожного смволу в рядку на протилежний
+
+// Наприклад якщо рядок 'JavaScript', то на виході має бути 'jAVA sCRIPT'
+
+// // Олд-скульний метод
+// const script = 'JavaScript';
+// const letters = script.split('');
+// console.log(letters);
+// let invertedString = '';
+
+// for (const letter of letters) {
+//     console.log(letter);
+
+//     if (letter === letter.toLowerCase()) {
+//         console.log('Its letter in lower case', letter);
+//         invertedString += letter.toUpperCase();
+//     } else {
+//         console.log('Its letter in upper case', letter);
+//         invertedString += letter.toLocaleLowerCase();
+//     }
+// }
+
+// console.log('invertedString :', invertedString);
+
+// // Нью-скульний метод
+// const script = 'JavaScript';
+// const letters = script.split('');
+// let invertedString = '';
+
+// console.log(letters);
+
+// for (const letter of letters) {
+//     console.log(letter);
+
+//     // invertedString = letter === letter.toLowerCase()
+//     //     ? invertedString += letter.toUpperCase()
+//     //     : invertedString += letter.toLocaleLowerCase();
+    
+//     // OR
+//         invertedString += letter === letter.toLowerCase()
+//         ? letter.toUpperCase()
+//         : letter.toLocaleLowerCase();
+// }
+
+// console.log('invertedString :', invertedString);
