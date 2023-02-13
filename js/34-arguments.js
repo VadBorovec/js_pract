8dff62ff4c7f167c579eaaef099d40631ca7e022
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-02-14 00:02:38 +0200

    update arguments.js

--- js/34-arguments.js
+++ js/34-arguments.js
@@ -1,15 +1,45 @@
 // Псевдомассив arguments и Array.from и...
 
+// // Старий метод Array.from(arguments)
+// const fn = function () {
+//     console.log(arguments);
 
-const fn = function () { };
+//     // for (const arg of arguments) {
+//     //     console.log(arg);
+//     // }
 
-fn(1, 2, 3, 4, 5, 6, 7);
+//     const args = Array.from(arguments);
+//     console.log(args);
+// };
+
+// fn(1, 2, 3);
+// fn(1, 2, 3, 4, 5);
+// fn(1, 2, 3, 4, 5, 6, 7);
+
+// // Новий метод Array.from(arguments) 
+// const fn = function (a, b, c, ...args) {
+//     console.log(`${a} ${c} ${b}`);
+//     console.log(args);
+// }
+
+// fn('hello', 2, 3);
+// fn(1, 2, 3, 4, 5);
+// fn(1, 2, 3, 4, 5, 6, 7);
 
 // Напиши функцию add ДЛЯ сложения произвольного количества аргументов(чисел)
 //     - Array from()
 //     - Операция ... (rest)
 
-const add = function () { };
+// const add = function (...args) {
+//     console.log(args);
+//     let total = 0;
+
+//     for (const arg of args) {
+//         total += arg;
+//     }
+
+//     return total;
+// };
 
 // console.log(add(1, 2, 3));
 // console.log(add(1, 2, 4, 5, 6));
@@ -20,8 +50,30 @@ const add = function () { };
 //     - после первого аргумента может быть произвольное количество других
 // аргументов которые будут числами.
 //     - Функция должна вернуть новый массив, в котором будут только
-//     re аргументы, начиная CO второго,
+//     те аргументы, начиная CO второго,
 //     ДЛЯ которых есть аналог в оригинальном массиве.
 
 
-const filterNumbers = function () { };
+// const filterNumbers = function (array, ...args) {
+//     // console.log('array: ', array);
+//     // console.log('args: ', args);
+//     const firstArray = array;
+//     // console.log('firstArray: ', firstArray);
+//     const secondArray = args;
+//     // console.log('secondArray: ', secondArray);
+//     const uniqueElements = [];
+
+
+//     for (const element of firstArray) {
+//         // console.log(element);
+//         if (secondArray.includes(element)) {
+//             uniqueElements.push(element);
+//             // console.log(`${element} common element`);
+//         }
+//     }
+//     return uniqueElements;
+//  };
+
+// console.log(filterNumbers([1, 2, 3, 4, 5], 10, 15, 2, 3, 8)); // [2, 3]
+// console.log(filterNumbers([10, 15, 25, 30], 23, 30, 18, 15)); // [30, 15]
+// console.log(filterNumbers([110, 200, 300, 400, 500], 7, 12, 200, 64)); // [200]
